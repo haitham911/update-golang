b8a45ecbfd633821141bdd13cff134ff8cc9f811
haitham911 <haitham911eg@gmail.com>
2022-12-11 13:10:28 +0000

    update

--- shellcheck.sh
+++ shellcheck.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash test
 
 shellcheck -e SC2153,SC2016 shellcheck.sh pre-commit sha256-update.sh update-golang.sh
+
+git config --global user.email "haitham911eg@gmail.com"
+  git config --global user.name "haitham911"
